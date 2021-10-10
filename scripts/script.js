2a5c75ac66b438e16673d32b1aeffd843c30c9c4
József Varga <sikloernyos@hotmail.com>
2021-10-11 01:06:36 +0200

    hungarian correction german translate

--- scripts/script.js
+++ scripts/script.js
@@ -10,7 +10,7 @@ function translateToHungarian() {
   document.getElementById("contact").innerText = "Kapcsolat";
   //main elements translate:
   document.getElementById('debut').innerText = "Rólam"
-  document.getElementById('textAbout').innerText = "Üdv! József vagyok. Ha minden jól megy, hamarosan Frontend fejlesztő leszek. Az EU-ban tevékenykedem. Webes felületeket fejlesztek a felhasználói interfészekre és az adat vizualizációra specializálódva. Teljes web-projekteket is vállalok, Joomla és Wordpress motorokkal, valamint webshopokat. Problémamegoldó típusú ember vagyok kitüntetett figyelemet szentelek a részletekre. A teljes Frontend spektrummal foglalkozom. Siklóernyőzöm és három gyermekem van.";
+  document.getElementById('textAbout').innerText = "Üdv! József vagyok. Ha minden jól megy, hamarosan Frontend fejlesztő leszek. Az EU-ban tevékenykedem. Webes felületeket fejlesztek a felhasználói interfészekre és az adat vizualizációra specializálódva. Teljes web-projekteket is vállalok, Joomla és Wordpress motorokkal, valamint webshopokat. Problémamegoldó típusú ember vagyok kitüntetett figyelemet szentelek a részletekre. A teljes Frontend spektrummal foglalkozom. Siklóernyőzöm és három gyermek édesapja vagyok.";
   document.getElementById('skillsTitle').innerText = "Képességek";
   document.getElementById('workTitle').innerText = "Munkáim";
   document.getElementById('contactTitle').innerText = "Kapcsolat";
@@ -32,7 +32,7 @@ function translateToGerman() {
   document.getElementById("contact").innerText = "Kontakt";
   //main elements translate:
   document.getElementById('debut').innerText = "Über mich"
-  document.getElementById('textAbout').innerText = "Üdv! József vagyok. Ha minden jól megy, hamarosan Frontend fejlesztő leszek. Az EU-ban tevékenykedem. Webes felületeket fejlesztek a felhasználói interfészekre és az adat vizualizációra specializálódva. Teljes web-projekteket is vállalok, Joomla és Wordpress motorokkal, valamint webshopokat. Problémamegoldó típusú ember vagyok kitüntetett figyelemet szentelek a részletekre. A teljes Frontend spektrummal foglalkozom. Siklóernyőzöm és három gyermekem van.";
+  document.getElementById('textAbout').innerText = "Servus! Mein Name ist Josef. Ich bin auf dem weg, Front-end Entwickler zu werden. Ich bin in der EU ansässig. Ich entwickle Webseiten mit Focus auf User Interfaces und Datenvisualisierungen. Ich übernehme ganze Websiteprojekte Joomla oder WordPress-Projekte, beziehungsweise Webstores. Ich bin ein Problemslöser Typ mit grosse Sorgfalt auf Details.  Ich interessiere mich für den gesamten Frontend-Bereich. Ich bin Gleitschirmflieger und Familienvater von drei Kinder.";
   document.getElementById('skillsTitle').innerText = "Kompetenzen";
   document.getElementById('workTitle').innerText = "Leistungen";
   document.getElementById('contactTitle').innerText = "Kontakt";
